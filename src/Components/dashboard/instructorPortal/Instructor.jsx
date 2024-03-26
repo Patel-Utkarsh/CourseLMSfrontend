e9fe19c2130bde0aa08682540ed70ecddf8e0b82
Utkarsh Patel <utkarshp04@gmail.com>
2024-03-26 18:29:27 +0530

    Update  121 Instructor.jsx

--- src/Components/dashboard/instructorPortal/Instructor.jsx
+++ src/Components/dashboard/instructorPortal/Instructor.jsx
@@ -1,70 +1,53 @@
-import "./instructor.css"
+import "./instructor.css";
 import { IoMdAddCircle } from "react-icons/io";
 import { MdDashboard } from "react-icons/md";
-import { FaEdit } from "react-icons/fa";
-import { FaBarsProgress } from "react-icons/fa6";
-import { useState,useContext } from "react";
-import { useParams } from "react-router-dom";
+import { FaEdit, FaBarsProgress } from "react-icons/fa";
+import { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import { AppProvider } from "../../../data";
 
-
-
 export default function Instructor(data) {
-      const { user} = useContext(AppProvider);
+    const { user } = useContext(AppProvider);
     const style = {
-        color  : '#a435f0'
-    }
- 
+        color: '#a435f0'
+    };
 
-    const [num,setNum] = useState(data.num);
+    const [selectedNum, setSelectedNum] = useState(data.num);
+
     return (
-
         <div className="InstructorWrapper">
-
             <div className="InstructorInnerDiv1">
-                <Link to={'/dashboard/MyAccount'}>
-               
-                <div  className={`InstructorInnerDiv2${num === 1? 'selected' : ''}`} onClick={()=>setNum(1)} >
-                    <MdDashboard style={style}></MdDashboard>
-                    <p>My Account</p>
-
-                </div>
-
+                <Link to='/dashboard/MyAccount'>
+                    <div className={`InstructorInnerDiv2 ${selectedNum === 1 ? 'selected' : ''}`} onClick={() => setSelectedNum(1)}>
+                        <MdDashboard style={style} />
+                        <p>My Account</p>
+                    </div>
                 </Link>
 
-                <Link to={'/myCourses'}>
-
-                <div  className={`InstructorInnerDiv2${num === 2? 'selected' : ''}`} onClick={()=>setNum(2)}>
-                    <FaBarsProgress style={style} className="pg"></FaBarsProgress>
-                    <p>My Learnings</p>
-
-
-                </div>
+                <Link to='/myCourses'>
+                    <div className={`InstructorInnerDiv2 ${selectedNum === 2 ? 'selected' : ''}`} onClick={() => setSelectedNum(2)}>
+                        <FaBarsProgress style={style} className="pg" />
+                        <p>My Learnings</p>
+                    </div>
                 </Link>
 
-               {user && user.account_type !== 'student' && (
+                {user && user.account_type !== 'student' && (
                     <>
-                        <Link to={'/dashboard/addNewCourse'}>
-                            <div className={`InstructorInnerDiv2${num === 3 ? 'selected' : ''}`} onClick={() => setNum(3)}>
-                                <IoMdAddCircle style={style}></IoMdAddCircle>
+                        <Link to='/dashboard/addNewCourse'>
+                            <div className={`InstructorInnerDiv2 ${selectedNum === 3 ? 'selected' : ''}`} onClick={() => setSelectedNum(3)}>
+                                <IoMdAddCircle style={style} />
                                 <p>Add Courses</p>
                             </div>
                         </Link>
-                        <Link to={'/dashboard/editCourse'}>
-                            <div onClick={() => setNum(4)} className={`InstructorInnerDiv2${num === 4 ? 'selected' : ''}`}>
-                                <FaEdit style={style}></FaEdit>
+                        <Link to='/dashboard/editCourse'>
+                            <div className={`InstructorInnerDiv2 ${selectedNum === 4 ? 'selected' : ''}`} onClick={() => setSelectedNum(4)}>
+                                <FaEdit style={style} />
                                 <p>Edit Course</p>
                             </div>
                         </Link>
                     </>
                 )}
-
             </div>
         </div>
-
-
-
     );
-
 }
